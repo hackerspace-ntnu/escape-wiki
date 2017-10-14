477f1728dbe2a691145d659a40c044bff120f732
Markus Ho-Yen <mahoyen@stud.ntnu.no>
2017-10-14 18:55:37 +0200

    Added Hint Model with viewmodification and blank hint.html

--- wiki/views.py
+++ wiki/views.py
@@ -2,20 +2,26 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
 
-from articles.models import Article
+from articles.models import Article, Hint
 
 
 class Index(View):
     def get(self, request, *args, **kwargs):
         try:
             context = {
-                'article': Article.objects.get(code=kwargs['url'])
+                'article': Hint.objects.get(code=kwargs['url'])
             }
-            return render(request, 'articles/article.html', context)
-        except Article.DoesNotExist:
-            return HttpResponseRedirect('/101')
+            return render(request, 'articles/hint.html', context)
+        except:
+            try:
+                context = {
+                    'article': Article.objects.get(code=kwargs['url'])
+                }
+                return render(request, 'articles/article.html', context)
+            except Article.DoesNotExist:
+                return HttpResponseRedirect('/101')
 
-        #return render(request, 'wiki/index.html')
+            #return render(request, 'wiki/index.html')
 
 
     def post(self, request, *args, **kwargs):
